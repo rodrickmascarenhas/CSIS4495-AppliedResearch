a70ea541164d95377ed6570c2133ba1d75086e0b
@rodrickmascarenhas <30309234+rodrickmascarenhas@users.noreply.github.com>
2022-10-31 01:12:28 -0700

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -29,7 +29,7 @@ def create_app():
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = 'rodster123sanfrancis@gmail.com'
     app.config['MAIL_DEFAULT_SENDER'] = 'rodster123sanfrancis@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'kinng2tonnn'
+    app.config['MAIL_PASSWORD'] = ''
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
